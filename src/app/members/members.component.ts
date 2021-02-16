bcc2048794ce4bdfe62f6b33b8f17f8019851ca0
yuga77 <y-n7c8@i.softbank.jp>
2021-02-16 20:35:14 +0900

    add check function

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -14,10 +14,15 @@ export class MembersComponent implements OnInit {
     id:   1,
     name: '田中太郎'
   }
+  selectedMember: Member;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(member: Member): void {
+    this.selectedMember = member;
   }
 
 }
