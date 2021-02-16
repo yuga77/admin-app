a5b1d97b8e01f3dac1436424e366aa15bb0c6e0e
yuga77 <y-n7c8@i.softbank.jp>
2021-02-16 20:25:20 +0900

    create member list

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Member }            from '../member';
+import { MEMBERS }           from '../mock-members';
 
 @Component({
   selector: 'app-members',
@@ -8,6 +9,7 @@ import { Member }            from '../member';
 })
 export class MembersComponent implements OnInit {
 
+  members = MEMBERS;
   member: Member = {
     id:   1,
     name: '田中太郎'
