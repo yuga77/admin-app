d01ca57e7fbb8590e8ec4091d3e25e6ed848c5e1
yuga77 <y-n7c8@i.softbank.jp>
2021-02-16 20:45:55 +0900

    cut component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule }   from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }     from './app.component';
 import { MembersComponent } from './members/members.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MembersComponent
+    MembersComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
