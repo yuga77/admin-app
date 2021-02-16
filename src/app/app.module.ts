0a4beaa6cadb171a1038c7d34a56b4977cc525f8
yuga77 <y-n7c8@i.softbank.jp>
2021-02-16 19:58:57 +0900

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/members/members.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+
+  member = '田中太郎';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
