c7c03cf5a16b95427696e64bc68c510a7a88aeed
yuga77 <y-n7c8@i.softbank.jp>
2021-02-17 22:53:15 +0900

    add function

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -44,6 +44,14 @@ export class MemberService {
       )
   }
 
+  addMember(member: Member): Observable<Member> {
+    return this.http.post<Member>(this.membersUrl, member, this.httpOptions)
+      .pipe(
+        tap((newMember: Member) => this.log(`社員データ(id=${newMember.id})を追加しました`)),
+        catchError(this.handleError<Member>('addMember'))
+      )
+  }
+
   private log(message: string) {
     this.messageService.add(`MemberService: ${message}`);
   }
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -23,4 +23,13 @@ export class MembersComponent implements OnInit {
       .subscribe(members => this.members = members);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return };
+    this.memberService.addMember({ name } as Member)
+      .subscribe(member => {
+        this.members.push(member);
+      });
+  }
+
 }
