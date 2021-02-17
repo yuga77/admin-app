7aa0d2b0e9f8f8dc8c46dc2d3b558ad73b9fc6e9
yuga77 <y-n7c8@i.softbank.jp>
2021-02-17 23:03:09 +0900

    add delete function

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -52,6 +52,17 @@ export class MemberService {
       )
   }
 
+  deleteMember(member: Member | number): Observable<Member> {
+    const id = typeof member === 'number' ? member : member.id;
+    const url = `${this.membersUrl}/${id}`;
+
+    return this.http.delete<Member>(url, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`社員データ(id=${id})を削除しました`)),
+        catchError(this.handleError<Member>('deleteMember'))
+      );
+  }
+
   private log(message: string) {
     this.messageService.add(`MemberService: ${message}`);
   }
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -32,4 +32,9 @@ export class MembersComponent implements OnInit {
       });
   }
 
+  delete(member: Member): void {
+    this.members = this.members.filter(m => m !== member);
+    this.memberService.deleteMember(member).subscribe();
+  }
+
 }
