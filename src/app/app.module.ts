6ebeb01d9fc798d541e88aa14e7c3b4595663696
yuga77 <y-n7c8@i.softbank.jp>
2021-02-17 22:29:31 +0900

    add error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule }      from '@angular/core';
-import { FormsModule }   from '@angular/forms'
+import { BrowserModule }                  from '@angular/platform-browser';
+import { NgModule }                       from '@angular/core';
+import { FormsModule }                    from '@angular/forms';
+import { HttpClientModule }               from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }            from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }     from './app.component';
@@ -20,6 +23,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation : false }
+    ),
     FormsModule
   ],
   providers: [],
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,23 +1,46 @@
-import { Injectable }     from '@angular/core';
-import { Member }         from './member';
-import { MEMBERS }        from './mock-members';
-import { Observable, of } from 'rxjs';
-import { MessageService } from './message.service';
+import { Injectable }           from '@angular/core';
+import { Member }               from './member';
+import { MEMBERS }              from './mock-members';
+import { Observable, of }       from 'rxjs';
+import { MessageService }       from './message.service';
+import { HttpClient }           from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
+  private membersUrl = "api/members";
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getMembers(): Observable<Member[]> {
-    this.messageService.add('MemberService: 社員一覧データを取得しました')
-    return of(MEMBERS);
+    return this.http.get<Member[]>(this.membersUrl)
+      .pipe(
+        tap(members => this.log('社員データを取得しました')),
+        catchError(this.handleError<Member[]>('getMembers', []))
+      );
   }
 
   getMember(id: number): Observable<Member> {
     this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
     return of(MEMBERS.find(member => member.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`MemberService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} 失敗: ${error.message}`);
+
+      return of(result as T);
+    }
   }
 }
